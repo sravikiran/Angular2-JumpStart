8567a398cef49d7ca5e5b01b104399c79895de47
Rabi Kiran <rabi_kiran@trimble.com>
2016-09-28 15:28:03 +0530

    Changed the way customer module is used in app routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,8 @@
 import { RouterModule, Routes } from '@angular/router';
+import CustomerModule from './customer/customer.module';
 
 const app_routes: Routes = [
-  { path: 'customers/:id', loadChildren: 'app/customer/customer.module'},
+  { path: 'customers/:id', loadChildren: () =>  CustomerModule },
   { path: '**', pathMatch:'full', redirectTo: '/customers' } //catch any unfound routes and redirect to home page
 ];
 
